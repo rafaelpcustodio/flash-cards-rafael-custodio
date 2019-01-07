0fb90809d90474083131f88fd46d9182a06bf19b
Rafael Custodio <rafael.custodio@dextra-sw.com>
2019-01-07 20:39:09 -0200

    feature: crate styles of new deck view

--- components/deck/NewDeckView.js
+++ components/deck/NewDeckView.js
@@ -1,12 +1,69 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native'
+
+import {purple, gray, white} from './../../utils/colors'
 
 export default class NewDeckView extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {text: ''};
+  }
   render() {
     return (
-        <View style={{flex: 1}}>
-            <Text>NewDeckView</Text>
+        <View style={styles.rows}>
+            <Text style={styles.questionText}>
+              What is the title of your new deck?
+            </Text>
+            <TextInput
+              style={styles.typeTxt}
+              placeholder='Deck Title'
+              onChangeText={(text) => this.setState({text})}
+            />
+            <TouchableOpacity style={styles.submitButton}>
+              <Text style={styles.submitButtonTxt}>
+                Submit
+              </Text>
+            </TouchableOpacity>
         </View>
     )
   }
 }
+
+const styles = StyleSheet.create({
+    rows:{
+      alignItems:'center',
+      flex:1,
+    },
+    typeTxt:{
+      paddingLeft:20,
+      paddingRight:20,
+      marginTop:100,
+      borderRadius:10,
+      borderWidth:1,
+      height:30,
+      width:300,
+    },
+    questionText:{
+      marginTop:200,
+      fontSize:50,
+      marginTop:100,
+      color:gray,
+      textAlign:'center',
+    },
+    submitButton:{
+      marginTop:50,
+      padding:10,
+      paddingLeft:30,
+      paddingRight:30,
+      height:45,
+      alignSelf:'center',
+      borderRadius:10,
+      backgroundColor:purple,
+      justifyContent:'center',
+    },
+    submitButtonTxt:{
+      color:white,
+      fontSize:22,
+      textAlign:'center'
+    }
+})
